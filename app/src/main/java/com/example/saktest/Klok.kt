9b6947c1d577157375c25c9f31344e797772c862
133479 <fgj>
2024-01-15 14:27:24 +0100

    added calender

--- app/src/main/java/com/example/saktest/Klok.kt
+++ app/src/main/java/com/example/saktest/Klok.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,26 +36,9 @@ class Klok : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_klok, container, false)
+        val view = inflater.inflate(R.layout.fragment_klok, container, false)
+        view.findViewById<Button>(R.id.Agendaknop).setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_klok_to_agenda)
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Klok.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Klok().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        return view
     }
 }
