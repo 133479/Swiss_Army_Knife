7bff8c826c87951c378346eb76c73325fd569e3a
133479 <fgj>
2024-01-22 14:29:20 +0100

    added a way to go to compas and a heading text and image

--- app/src/main/java/com/example/saktest/kompas.kt
+++ app/src/main/java/com/example/saktest/kompas.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Klok.newInstance] factory method to
+ * Use the [kompas.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Klok : Fragment() {
+class kompas : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,11 +34,26 @@ class Klok : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_klok, container, false)
-        view.findViewById<Button>(R.id.Agendaknop).setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_klok_to_agenda)}
+        return inflater.inflate(R.layout.fragment_kompas, container, false)
+    }
 
-        view.findViewById<Button>(R.id.kompasknop).setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_klok_to_kompas) }
-
-        return view
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment kompas.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            kompas().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
     }
 }
