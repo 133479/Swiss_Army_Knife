08d6da19b7a86ec4aecde9e6ee60c159c9c1efe7
133479 <133479@hondsrugcollege.nl>
2024-02-01 15:15:24 +0100

    added way back from kompas

--- app/src/main/java/com/example/saktest/kompas.kt
+++ app/src/main/java/com/example/saktest/kompas.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -31,12 +33,16 @@ class kompas : Fragment() {
         }
     }
 
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_kompas, container, false)
+        val view = inflater.inflate(R.layout.fragment_kompas, container, false)
+        view.findViewById<Button>(R.id.btn_kompas_klok).setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_kompas_to_klok)
+        }
+        return view
     }
 
     companion object {
@@ -59,4 +65,5 @@ class kompas : Fragment() {
             }
     }
 }
+
 
