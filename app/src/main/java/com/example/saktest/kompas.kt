75943980ead664076762d8544687aaae623be170
133479 <133479@hondsrugcollege.nl>
2024-02-05 14:28:19 +0100

    added accelerometer

--- app/src/main/java/com/example/saktest/kompas.kt
+++ app/src/main/java/com/example/saktest/kompas.kt
@@ -1,12 +1,21 @@
 package com.example.saktest
 
+import android.app.Activity
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.findFragment
 import androidx.navigation.Navigation
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,15 +27,19 @@ private const val ARG_PARAM2 = "param2"
  * Use the [kompas.newInstance] factory method to
  * create an instance of this fragment.
  */
-class kompas : Fragment() {
+
+
+
+class kompas : Fragment(), SensorEventListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-
+    private var mSensorManager: SensorManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mSensorManager = this.activity!!.getSystemService(Activity.SENSOR_SERVICE) as SensorManager
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -39,31 +52,67 @@ class kompas : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_kompas, container, false)
+        var view = inflater.inflate(R.layout.fragment_kompas, container, false)
         view.findViewById<Button>(R.id.btn_kompas_klok).setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_kompas_to_klok)
         }
+        mSensorManager!!.registerListener(this,
+            mSensorManager!!.getSensorList(Sensor.TYPE_ACCELEROMETER)[0], SensorManager.SENSOR_DELAY_FASTEST);
+
         return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment kompas.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            kompas().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onSensorChanged(event: SensorEvent) {
+        val x = event.values[0]
+        val y = event.values[1]
+        Log.println(Log.INFO, "SAK", "X is ${x.toString()}")
+        Log.println(Log.INFO, "SAK", "Y is ${y.toString()}")
+
+        //this.view.findViewById<TextView>(R.id.compasheading).text = x.toString();
     }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+    override fun setMenuVisibility(menuVisible: Boolean) {
+        super.setMenuVisibility(menuVisible)
+
+        // First starts (gets called before everything else)
+        if (mSensorManager == null) {
+            return
+        }
+        if (menuVisible) {
+            registerSensorListener()
+        } else {
+            unregisterSensorListener()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (this.userVisibleHint) {
+            registerSensorListener()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterSensorListener()
+    }
+
+    private fun registerSensorListener() {
+        mSensorManager!!.registerListener(
+            this,
+            mSensorManager!!.getSensorList(Sensor.TYPE_ACCELEROMETER)[0],
+            SensorManager.SENSOR_DELAY_FASTEST
+        )
+    }
+
+    private fun unregisterSensorListener() {
+        mSensorManager!!.unregisterListener(this)
+    }
+
+
+
+
 }
 
 
